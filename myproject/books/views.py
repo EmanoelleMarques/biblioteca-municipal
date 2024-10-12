c61f7bff0b6d2a97d4498c65a79957b9b4feb148
Lucas Jesus <lucas.aj2005@gmail.com>
2024-10-12 13:33:19 -0300

    delete all

--- myproject/books/views.py
+++
@@ -1,5 +0,0 @@
-import requests
-from django.shortcuts import render
-from .models import Book
-from .views import fetch_books_from_google
-
--- myproject/google_books/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import book_search
-
-urlpatterns = [
-    path("search/", book_search, name="book_search"),
-]
